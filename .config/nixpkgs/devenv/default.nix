ccda9b6aa538b2854575f0bd159557076e4df181
Artur Cygan <arczicygan@gmail.com>
2020-11-16 13:52:48 +0100

    neuron notes

--- .config/nixpkgs/devenv/default.nix
+++ .config/nixpkgs/devenv/default.nix
@@ -1,11 +1,16 @@
 { neovim-artur, tmux-artur, fish-artur, pkgs }:
 with pkgs;
-pkgs.buildEnv {
+let neuron-notes =
+  (let neuronRev = "15e482e5e0a876fb3fe42936d9992e91d5f0c98a";
+       neuronSrc = builtins.fetchTarball "https://github.com/srid/neuron/archive/${neuronRev}.tar.gz";
+    in import neuronSrc {});
+in pkgs.buildEnv {
   name = "devenv-artur";
   paths = [
     neovim-artur
     tmux-artur
     fish-artur
+    neuron-notes
     python3
 
     # Fish and nix-env compatibility
