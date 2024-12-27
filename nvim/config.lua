ef20798f4b309a7a5f5046808cb315182f36a630
Artur Cygan <arczicygan@gmail.com>
2024-12-27 16:07:38 +0100

    Bump nixpkgs

--- nvim/config.lua
+++ nvim/config.lua
@@ -1,41 +1,46 @@
 vim.cmd('packadd nvim-lspconfig')
 
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local bufopts = { noremap=true, silent=true, buffer=event.buf }
 
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<c-]>', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gtd', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', 'gr', require'telescope.builtin'.lsp_references, bufopts)
-  vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gds', require'telescope.builtin'.lsp_document_symbols, bufopts)
-  vim.keymap.set('n', 'gws', require'telescope.builtin'.lsp_workspace_symbols, bufopts)
-  vim.keymap.set('n', 'cd', vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set('n', 'gtd', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', 'gr', require'telescope.builtin'.lsp_references, bufopts)
+    vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gds', require'telescope.builtin'.lsp_document_symbols, bufopts)
+    vim.keymap.set('n', 'gws', require'telescope.builtin'.lsp_workspace_symbols, bufopts)
+    vim.keymap.set('n', 'cd', vim.diagnostic.goto_next, bufopts)
 
-  vim.api.nvim_create_autocmd("CursorHold", {
-    pattern = "*",
-    callback = function()
-      vim.diagnostic.open_float(nil, { focusable = false })
-    end,
-  })
+    vim.api.nvim_create_autocmd("CursorHold", {
+      pattern = "*",
+      callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+      end,
+    })
 
-  --vim.api.nvim_command [[autocmd CursorHold,CursorHoldI,InsertLeave <buffer> lua vim.lsp.codelens.refresh()]]
-  vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI", "InsertLeave"}, {
-    pattern = "*",
-    callback = function()
-      vim.lsp.codelens.refresh()
-    end,
-  })
-  vim.keymap.set("n", "cl", vim.lsp.codelens.run, bufopts)
-end
+    vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI", "InsertLeave"}, {
+      pattern = "*",
+      callback = function()
+        vim.lsp.codelens.refresh()
+      end,
+    })
+    vim.keymap.set("n", "cl", vim.lsp.codelens.run, bufopts)
+  end
+})
 
-require('lspconfig').rust_analyzer.setup({ on_attach = on_attach })
-require('lspconfig').ts_ls.setup({ on_attach = on_attach })
-require('lspconfig').gopls.setup({ on_attach = on_attach })
-require('lspconfig').hls.setup({ on_attach = on_attach })
-require('lspconfig').ccls.setup({ on_attach = on_attach })
+require('lspconfig').rust_analyzer.setup({
+  diagnostics = {
+    enable = true,
+    experimental = {
+      enable = true,
+    },
+  },
+})
+require('lspconfig').ts_ls.setup({ })
+require('lspconfig').gopls.setup({ })
+require('lspconfig').hls.setup({ })
+require('lspconfig').ccls.setup({ })
 
 require('lualine').setup({
   options = {
